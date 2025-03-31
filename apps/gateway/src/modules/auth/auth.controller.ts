268bd86414ce3c80f5b333b97e96d97edd2cdfa6
“Anupam <anupam@pitangent.com>
2025-03-31 15:00:56 +0530

    add

--- apps/gateway/src/modules/auth/auth.controller.ts
+++ apps/gateway/src/modules/auth/auth.controller.ts
@@ -1,65 +1,72 @@
 import { Controller, Post, Body, Put, Get, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
+import { Observable } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() payload: object, @Res({ passthrough: true }) res: Response) {
+  register(
+    @Body() payload: object,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Observable<object>> {
     return this.authService.register(payload, res);
   }
 
   @Post('login')
-  login(@Body() payload: object, @Res({ passthrough: true }) res: Response) {
+  login(
+    @Body() payload: object,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Observable<object>> {
     return this.authService.login(payload, res);
   }
 
   @Get('current-user')
-  getLoginUser(@Body() payload: object) {
-    this.authService.getLoginUser(payload);
+  getLoginUser(@Body() payload: object): Promise<Observable<object>> {
+    return this.authService.getLoginUser(payload);
   }
 
   @Get('refresh-token')
   getRefreshToken(
     @Body() payload: object,
     @Res({ passthrough: true }) res: Response,
-  ) {
-    this.authService.getRefreshToken(payload, res);
+  ): Promise<Observable<object>> {
+    return this.authService.getRefreshToken(payload, res);
   }
 
   @Put('change-password')
-  changePassword(@Body() payload: object) {
-    this.authService.changePassword(payload);
+  changePassword(@Body() payload: object): Promise<Observable<object>> {
+    return this.authService.changePassword(payload);
   }
 
   @Put('verify-email')
-  verifyEmail(@Body() payload: object) {
-    this.authService.verifyEmail(payload);
+  verifyEmail(@Body() payload: object): Promise<Observable<object>> {
+    return this.authService.verifyEmail(payload);
   }
 
   @Put('verify-otp/:otp')
   verifyOtp(
     @Body() payload: object,
     @Res({ passthrough: true }) res: Response,
-  ) {
-    this.authService.verifyOTP(payload, res);
+  ): Promise<Observable<object>> {
+    return this.authService.verifyOTP(payload, res);
   }
 
   @Get('resend-email')
   resendVerifyEmail(@Body() payload: object) {
-    this.authService.resendVerifyEmail(payload);
+    return this.authService.resendVerifyEmail(payload);
   }
 
   @Put('forgot-password')
-  forgotPassword(@Body() payload: object) {
-    this.authService.forgotPassword(payload);
+  forgotPassword(@Body() payload: object): Promise<Observable<object>> {
+    return this.authService.forgotPassword(payload);
   }
 
   @Put('reset-password/:token')
-  resetPassword(@Body() payload: object) {
-    this.authService.resetPassword(payload);
+  resetPassword(@Body() payload: object): Promise<Observable<object>> {
+    return this.authService.resetPassword(payload);
   }
 
   @Post('logout')
